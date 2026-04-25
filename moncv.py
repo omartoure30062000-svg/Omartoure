0310531a81cdb9f17ce084d8af8576ffbef776e9
omartoure30062000-svg <omartoure30062000@gmail.com>
2026-04-25 17:20:35 +0000

    Add files via upload

---
+++ moncv.py
@@ -0,0 +1,44 @@
+import streamlit as st
+st.info("PROFIL")
+st.text("Technicien Supérieur en Géomatique et Maitre en Géographie rurale,je combine expertise scientique et compétences numériques avancées.rigoureux et polyvalent,j'ai l'ambition de mettre mon double savoir-faire au service de votre entreprise afin de contribuer activement à la réussite de vos projets.")
+
+with st.sidebar:
+    st.header("OMAR TOURE")
+    st.subheader("Technicien Supérieur en Géomatique et Maitre en Géographie")
+    st.subheader("Mail")
+    st.write("omartoure200120@gmail.com")
+    st.subheader("ADRESSE")
+    st.write("Fatick,Sénégal")   
+
+st.info("FORMATION")
+st.markdown("""
+* Master Gestion et Developpement des Espaces Ruraux 2025-2026  
+* licence en Géographie Humaine 2023-2024
+* Brévet de Technicien Supérieur en Geomatique 2025-2026
+* Baccarauréat en Sciences Humaines et Sociales 2020-2021
+""")
+
+st.info("COMPETENCES")
+st.markdown("""
+
+* Maitrise des outils des Systèmes d'Information Géographique
+* Maitrise des outils de la Cartographie(Qgis, Argis)
+* Informatique(Word,Excel,Power point)
+* Analyse spatiale
+* Collecte de donnees de terrain
+* Maitrise des statistiques
+* Maitrise des outils dessin architectural
+* Topographie
+""")
+st.info("CERTIFICATIONS")
+ 
+   
+    
+    
+    
+
+
+    
+
+
+
